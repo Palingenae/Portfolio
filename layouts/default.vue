041fa1336e3271285d7a42a9ccd639b54bc0c16b
Olivia De Scheerder <6728594+Palingenae@users.noreply.github.com>
2020-08-03 19:12:05 +0200

    init

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <Nuxt />
+</template>
+
+<style lang="scss">
+html {
+  font: {
+    family: $font-text, -apple-system, BlinkMacSystemFont, sans-serif;
+    size: 20px;
+  }
+  color: $color-grey-9;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+body {
+  background-color: $color-grey-1;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="main">
+    <h1>
+      Bonjour !
+    </h1>
+    <h2>
+      Je m'appelle Olivia.
+    </h2>
+    <p>
+      Je suis une développeuse Web et Photographe actuellement basée à Nantes.
+    </p>
+    <!-- Liens vers lers réseaux sociaux-->
+    <!-- Vers le bas -->
+  </main>
+</template>
